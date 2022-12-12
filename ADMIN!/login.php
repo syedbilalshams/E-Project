20b5ec586f154694e621824c820c3c39c0ce5556
usman <usmanshams31@gmail.com>
2022-12-12 20:02:35 +0500

    all

--- ADMIN!/login.php
+++ ADMIN!/login.php
@@ -58,7 +58,7 @@ if(isset($_SESSION['user'])){
                     <div class="bg-light rounded p-4 p-sm-5 my-4 mx-3">
                         <div class="d-flex align-items-center justify-content-between mb-3">
                             <a href="index.html" class="">
-                                <h3 class="text-primary"><i class="fa fa-hashtag me-2"></i>DASHMIN</h3>
+                                <h3 class="text-primary"><i class="fa fa-hashtag me-2"></i>COVID.</h3>
                             </a>
                             <h3>Sign In</h3>
                         </div>
